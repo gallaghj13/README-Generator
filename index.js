12e00668b9b0e0120f087469c3ba5a006ecc75da
John Gallagher <gallagherj0913@gmail.com>
2021-10-24 22:06:24 -0600

    started questions and the function to initialize the app

--- index.js
+++ index.js
@@ -2,13 +2,68 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description of the project:',
+      },
+      {
+        type: 'input',
+        name: 'contents',
+        message: '',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: '',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: '',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: '',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: '',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: '',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: '',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt (questions)
+        .then((answers) => {
+            const readmePageContent = writeToFile(answers);
+
+            fs.writeFile('README.md', readmePageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md!'))
+        })
+}
 
 // Function call to initialize app
 init();
