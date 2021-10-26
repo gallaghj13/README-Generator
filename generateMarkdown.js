6f37606e63f35bf9727b14e206db08af449d6888
John Gallagher <gallagherj0913@gmail.com>
2021-10-25 22:31:03 -0600

    first draft on the generateMarkdown function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -14,6 +14,40 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description {#description}
+
+  ${data.description}
+
+  ## Table of Contents
+  1. [Description] (#description)
+  2. [Installation] (#installation)
+  3. [Usage] (#usage)
+  4. [Contributing] (#contributing)
+  5. [Tests] (#tests)
+  6. [Questions] (#questions)
+
+  ## Installation {#installation}
+
+  ${data.installation}
+
+  ## Usage {#usage}
+
+  ${data.usage}
+
+  ## Contributing {#contributing}
+
+  ${data.contributing}
+
+  ## Tests {#tests}
+
+  ${data.tests}
+
+  ## Questions {#questions}
+
+  Follow link for github: [Github Profile] (https://github.com/${data.github})
+  For any additional questions you can reach me at:
+  Email: ${data.email}
+
 `;
 }
 
