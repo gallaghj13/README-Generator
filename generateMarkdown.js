42e44c7af3de90dd54b79bd715cd6aede8ef526d
John Gallagher <gallagherj0913@gmail.com>
2021-10-26 00:56:23 -0600

    added return of empty strings for no license present

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+// Apache 2.0 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+// Mozilla [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+// GNU [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+function renderLicenseBadge(license) {
+    if (license === []) {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license === []) {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license === []) {
+        return '';
+    }
+}
 
 // {title, description, installation, usage, contributing, tests, github, email}
 // TODO: Create a function to generate markdown for README
