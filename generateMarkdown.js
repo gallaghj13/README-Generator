955ff25a922b4c40557102c5b15a6ce9d27a781c
John Gallagher <gallagherj0913@gmail.com>
2021-10-26 01:46:22 -0600

    got license badge to work for MIT

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -4,11 +4,24 @@
 // Apache 2.0 [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 // Mozilla [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
 // GNU [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-function renderLicenseBadge(license) {
+
+function renderLicenseBadge(license){
     if (license === []) {
         return '';
     }
-}
+    if (license[0] === 'MIT') {
+        return "## License: " + "<br />" + "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    }
+    if (license[0] === 'Apache License 2.0') {
+        console.log("Apache");
+    }
+    if (license[0] === 'Mozilla Public License 2.0') {
+        console.log("Mozilla");
+    }
+    if (license[0] === 'GNU General Public License') {
+        console.log("GNU");
+    }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -28,12 +41,14 @@ function renderLicenseSection(license) {
 
 // {title, description, installation, usage, contributing, tests, github, email}
 // TODO: Create a function to generate markdown for README
-function generateMarkdown({title, description, installation, usage, contributing, tests, github, email}) {
-  return `# ${title}
+function generateMarkdown({title, description, installation, usage, contributing, tests, github, email, license}) {
+return `# ${title}
 
   ## Description
 
   ${description}
+
+  ${renderLicenseBadge(license)}
 
   ## Table of Contents
   1. [Description](#description)
@@ -69,3 +84,4 @@ function generateMarkdown({title, description, installation, usage, contributing
 }
 
 module.exports = generateMarkdown;
+
